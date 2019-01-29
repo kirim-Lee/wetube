2c121d5a2876b891dd3ae189f2656f205b13eb1b
Lee Kirim <kirim@sk.com>
2019-01-29 09:52:04 +0900

    babel use

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-function handleHome(_req,res) {
-    res.send('GET request to the homepage');
-}
-function handleProfile(_req, res) {
-    res.send('You are on my porfile');
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (_req,res) => res.send('GET request to the homepage!');
+const handleProfile = (_req, res) => res.send('You are on my porfile!');
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
