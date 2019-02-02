f3b6617c2e01a951b6da8c99ba0d5f5bb65a472c
Lee Kirim <kirim@sk.com>
2019-02-02 23:02:37 +0900

    query string 얻기

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,5 +1,8 @@
 export const home = (req, res) => res.render("home", {pageTitle: 'Home'});
-export const search = (req, res) => res.render("search");
+export const search = (req, res) => {
+    const {term: searchingBy} = req.query;
+    return res.render("search", {pageTitle: 'Search', searchingBy});
+}
 export const upload = (req, res) => res.render("upload");
 export const videoDetail = (req, res) => res.render("videoDetail");
 export const editVideo = (req, res) => res.render("editVideo");
