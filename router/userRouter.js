c5508a8a1d13cfc6672e54315aebccb1b3dca180
Lee Kirim <kirim@sk.com>
2019-02-02 23:45:43 +0900

    edit profile page view

--- router/userRouter.js
+++ router/userRouter.js
@@ -4,8 +4,8 @@ import { users, editProfile, changePassword, userDetail } from '../controllers/u
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail, userDetail );
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail );
 
 export default userRouter;
