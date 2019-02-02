6af827fcb290c03b002a1778157b16c48aa8b5a2
Lee Kirim <kirim@sk.com>
2019-02-02 22:43:23 +0900

    view 에 local variable 전달방법

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ import globalRouter from './router/globalRouter';
 import userRouter from './router/userRouter';
 import videoRouter from './router/videoRouter';
 import routes from './routes';
+import { localsMiddleware } from './middlewares';
 const app = express();
 
 app.set('view engine', 'pug');
 // middleware
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use('/', globalRouter);
 app.use(routes.users, userRouter);
