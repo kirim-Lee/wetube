d505b9ccff2ed58e7a03ddf0f3f365ac2b497b74
Lee Kirim <kirim@sk.com>
2019-02-03 11:22:04 +0900

    join controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,19 @@
-export const join = (req, res) => res.render("join");
+import routes from '../routes';
+
+export const getJoin = (req, res) => {
+    return res.render("join", {pageTitle: 'Join'});
+}
+export const postJoin = (req, res) => {
+    const {body: {password, password2, name, email}} = req;
+    if(password !== password2) {
+        res.status(400);
+        return res.render("join", {pageTitle: 'Join'});
+    } else {
+        // To Do: Register User
+        // To Do: Log in
+        return res.redirect(routes.home);
+    }
+}
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
 export const userDetail = (req, res) => res.render("userDetail");
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -5,7 +5,7 @@ export const home = (req, res) => {
 }
 export const search = (req, res) => {
     const {term: searchingBy} = req.query;
-    return res.render("search", {pageTitle: 'Search', searchingBy});
+    return res.render("search", {pageTitle: 'Search', searchingBy, videos});
 }
 export const upload = (req, res) => res.render("upload");
 export const videoDetail = (req, res) => res.render("videoDetail");
