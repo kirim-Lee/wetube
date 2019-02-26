6eca2a86d333dbfcf273f169f85598cfa272d38e
Lee Kirim <kirim@sk.com>
2019-02-26 14:12:22 +0900

    video mute

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,8 @@ module.exports = {
         "indent": [2, 4],
         "ImportDeclaration": "always", 
         "ExportDeclaration": "never" 
+    },
+    "env": {
+        "browser": true // document 가 에러 없이 사용할 수 있게 해줌
     }
 };
--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,20 +1,33 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
-let videoPlayer, playBtn;
+let videoPlayer, playBtn, volumnBtn;
 
 function getElements() {
     videoPlayer = videoContainer.querySelector('video');
     playBtn = videoContainer.querySelector('#jsPlayButton');
+    volumnBtn = videoContainer.querySelector('#jsVolumnButton');
 }
 
 function init () {    
     playBtn.addEventListener('click', handlePlayClick);
+    volumnBtn.addEventListener('click', handleVolumnClick);
 }
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
-        videoPlayer.play()
+        videoPlayer.play();
+        playBtn.innerHTML = '<i class="fas fa-pause"></i>';
     } else {
         videoPlayer.pause();
+        playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+}
+
+function handleVolumnClick() {
+    videoPlayer.muted = !videoPlayer.muted;
+    if(videoPlayer.muted) {
+        volumnBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    } else {
+        volumnBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
     }
 }
 
