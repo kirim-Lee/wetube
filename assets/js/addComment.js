62648d0a1fb6df3212b03f0157bcf0820bb46e6a
Lee Kirim <kirim@sk.com>
2019-04-08 10:18:37 +0900

    add comment fake update

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,12 +1,28 @@
 import axios from 'axios';
 
 const addCommentForm = document.getElementById('jsAddComment');
+const commentList = document.getElementById('jsCommentList');
+const commentNumber = document.getElementById('jsCommentNumber');
 
+const increaseNumber = () => {
+    const prevNumber = parseInt(commentNumber.textContent, 10);
+    commentNumber.innerText = prevNumber + 1;
+}
+
+const addComment = (comment) => {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerHTML = comment;
+    li.appendChild(span);
+    commentList.prepend(li);
+    increaseNumber();
+}
 
 const handleSubmitComment = (event) => {
     event.preventDefault();    
     const commentInput = addCommentForm.querySelector('input');
     const comment = commentInput.value;    
+    commentInput.value = '';
     sendComment(comment);
 }
 
@@ -19,7 +35,9 @@ const sendComment = async (comment) => {
             comment: comment
         }
     });
-    console.log(response);
+    if (response.status === 200) {
+        addComment(comment);
+    }
 }
 
 function init() {
