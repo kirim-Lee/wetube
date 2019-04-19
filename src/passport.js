e277badc2b87e156dba5928493403b3703c676c6
Lee Kirim <kirim@sk.com>
2019-04-19 13:47:00 +0900

    set prod

--- src/passport.js
+++ src/passport.js
@@ -10,13 +10,17 @@ passport.use(User.createStrategy());
 passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: `http://localhost:4000${routes.githubCallback}`
+    callbackURL: process.env.PRODUCTION ? 
+        `https://safe-journey-47768.herokuapp.com${routes.githubCallback}` : 
+        `http://localhost:4000${routes.githubCallback}`
 }, githubLoginCallback));
 
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: `http://localhost:4000${routes.fbCallback}`,
+    callbackURL: process.env.PRODUCTION ? 
+        `https://safe-journey-47768.herokuapp.com${routes.fbCallback}` : 
+        `http://localhost:4000${routes.fbCallback}`,
     profileFields: ['id', 'displayName', 'email']
 }, fbLoginCallback));
 
